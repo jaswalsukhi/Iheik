04d56585b55ba7c7aa2df47582f00699bf44af3c
jaswalsukhi <jaswalsukhi@hotmail.com>
2014-04-03 15:42:43 +1100

    made structure oky

---
+++ main/Iheik.Domain/Source/Pocos/UserEntityPoco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iheik.Domain.Pocos
+{
+    public class UserEntityPoco
+    {
+        public int UserCustomerEntityId { get; set; }
+        public int CustomerUsersId { get; set; }
+        public int CustomerEntityId { get; set; }
+        public string CustomerEntityCode { get; set; }
+        public string CustomerEntityName { get; set; }
+        public bool ReadOnly { get; set; }
+    }
+}
