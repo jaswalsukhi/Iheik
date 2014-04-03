14e0af9301c5b16c99cebe8765bb737497f7804f
jaswalsukhi <jaswalsukhi@hotmail.com>
2014-04-04 09:40:02 +1100

    restructuring

---
+++ main/Iheik.Web/Source/Extensions/HtmlLabelExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Linq.Expressions;
+
+namespace Iheik.Extensions
+{
+    public static class HtmlLabelExtension
+    {
+
+        public static MvcHtmlString LabelForWithAttributes<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, object htmlAttributes)
+        {
+            return LabelForWithAttributes(html, expression, new RouteValueDictionary(htmlAttributes));
+        }
+
+        public static MvcHtmlString LabelForWithAttributes<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, IDictionary<string, object> htmlAttributes)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+            string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+            string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+            if (String.IsNullOrEmpty(labelText))
+            {
+                return MvcHtmlString.Empty;
+            }
+            TagBuilder tag = new TagBuilder("label");
+            tag.MergeAttributes(htmlAttributes);
+            tag.Attributes.Add("for", html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
+            tag.SetInnerText(labelText);
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+
+        }
+
+    }
+
+
+}
---
+++ main/Iheik.Web/Source/Helpers/UserAccessHelper.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Iheik.Helpers
+{
+
+    /// <summary>
+    /// Basically this class exposes the User Credential properties to UI pages, 
+    /// so the Membership object is not to exposed directly (cleaner).
+    /// </summary>
+    public static class UserAccessHelper
+    {
+
+        /// <summary>
+        /// Property for checking whether a user is logged on
+        /// </summary>
+        static public bool UserLoggedOn
+        {
+            get
+            {
+                return IsCurrentUserLoggedOn();
+            }
+        }
+
+        /// <summary>
+        /// Checks if a user has an active session
+        /// </summary>
+        /// <returns>Boolean - True if current user is logged in.</returns>
+        private static bool IsCurrentUserLoggedOn()
+        {
+            bool userLoggedOn = false;
+
+            userLoggedOn = HttpContext.Current.User.Identity.IsAuthenticated;
+
+            return userLoggedOn;
+        }
+
+        /// <summary>
+        /// Property for checking whether a user is an administrator 
+        /// </summary>
+        static public bool UserIsAdmin
+        {
+            get
+            {
+                return IsCurrentUserAdmin();
+            }
+        }
+
+        /// <summary>
+        /// Checks if a user is an administrator
+        /// </summary>
+        /// <returns>Boolean - True if current user is admin.</returns>
+        private static bool IsCurrentUserAdmin()
+        {
+            bool userIsAdmin = false;
+
+            userIsAdmin = HttpContext.Current.User.IsInRole("GetRoleNamefromConfig");
+
+            return userIsAdmin;
+        }
+
+        /// <summary>
+        /// Property for checking whether a user is a system administrator 
+        /// </summary>
+        static public bool UserIsSysAdmin
+        {
+            get
+            {
+                return IsCurrentUserSysAdmin();
+            }
+        }
+
+        /// <summary>
+        /// Checks if a user is a system administrator
+        /// </summary>
+        /// <returns>Boolean - True if current user is sys admin.</returns>
+        private static bool IsCurrentUserSysAdmin()
+        {
+            bool userIsAdmin = false;
+
+            userIsAdmin = HttpContext.Current.User.IsInRole("GetRoleNamefromConfig");
+
+            return userIsAdmin;
+        }
+
+        /// <summary>
+        /// Exposes name of the user.
+        /// </summary>
+        static public string UserName
+        {
+            get
+            {
+                return GetUserName();
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the user.
+        /// </summary>
+        /// <returns>Logged in User Name.</returns>
+        private static string GetUserName()
+        {
+            string userName = string.Empty;
+
+            if (IsCurrentUserLoggedOn())
+            {
+                userName = HttpContext.Current.User.Identity.Name;
+            }
+
+            return userName;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether to show detailed error.
+        /// </summary>
+        static public bool ShowDetailedError
+        {
+            get
+            {
+                return DetailedErrorAreOn();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether to Show Detailed Errors.
+        /// </summary>
+        /// <returns>Boolean - True for showing a detail error stack on page.</returns>
+        private static bool DetailedErrorAreOn()
+        {
+            bool showDetailedError = false;
+
+            showDetailedError = false; //"GetSettingFromConfig";
+
+            return showDetailedError;
+        }
+
+    }
+}
